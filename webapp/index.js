4b1b34db9201975c4d1618a9b50eaec290bb2cb5
Stephanie <36951771+liyeez@users.noreply.github.com>
2021-05-12 15:38:32 +1000

    Some basic fragments

--- webapp/index.js
+++ webapp/index.js
@@ -1,8 +1,17 @@
 sap.ui.define([
 	"sap/m/Button",
-	"sap/m/MessageToast"
-], function (Button, MessageToast) {
+	"sap/m/MessageToast",
+    "sap/ui/core/mvc/XMLView"
+
+], function (Button, MessageToast, XMLView) {
 	"use strict";
+    
+    XMLView.create({viewName: "Quickstart.App"}).then(function (oView){
+        oView.placeAt("content")
+    });
+
+    //  introduce a proper XML view to separate the presentation from the controller logic. 
+    //  We prefix the view name Quickstart.App with our newly defined namespace. The view is loaded asynchronously.
 
 	new Button({
 		text: "Ready...",
@@ -12,3 +21,14 @@ sap.ui.define([
 	}).placeAt("content");
 
 });
+
+
+// sap.ui.define([
+// 	"sap/ui/core/mvc/XMLView"
+// ], function (XMLView) {
+// 	"use strict";
+
+// 	XMLView.create({viewName: "Quickstart.App"}).then(function (oView) {
+// 		oView.placeAt("content");
+// 	});
+// });
